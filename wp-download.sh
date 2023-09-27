d5655a4262a90e31e6572beff956f90999fa52a4
wpdevenv <133993395+wpdevenv@users.noreply.github.com>
2023-09-27 17:50:53 +0300

    Update wp-download.sh

--- wp-download.sh
+++ wp-download.sh
@@ -2,6 +2,9 @@
 
 set -eux
 
+WP_CORE_DIR="${INSTALL_DIR:-html}"
+TMPDIR="${TMPDIR:-/tmp}"
+TMPDIR=$(echo $TMPDIR | sed -e "s/\/$//")
 TYPE="$1"
 SLUG="$2"
 VERSION="$3"
@@ -9,8 +12,8 @@ PACKAGE="${SLUG}.${VERSION}.zip"
 
 echo "Downloading $PACKAGE"
 
-curl -o "/tmp/$PACKAGE" -sL "https://downloads.wordpress.org/$TYPE/$PACKAGE"
+curl -o "$TMPDIR/$PACKAGE" -sL "https://downloads.wordpress.org/$TYPE/$PACKAGE"
 
-unzip -o "/tmp/$PACKAGE" -d "wp-content/${TYPE}s"
+unzip -o "$TMPDIR/$PACKAGE" -d "${WP_CORE_DIR}/wp-content/${TYPE}s"
 
-rm "/tmp/$PACKAGE"
+rm "$TMPDIR/$PACKAGE"
